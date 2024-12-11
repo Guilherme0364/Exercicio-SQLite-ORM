db967b0412beb6cc9d6754f96dbd84e844cc8bf6
Guilherme0364 <guilhermedg364@outlook.com>
2024-12-11 20:52:18 -0300

    refact folders and add paranoid in models

--- src/database/models/curso.js
+++ src/database/models/curso.js
@@ -29,6 +29,7 @@ module.exports = (sequelize, DataTypes) => {
 		sequelize,
 		modelName: 'Curso',
 		tableName: 'cursos'
+		paronoid: true
 	});
 	return Curso;
 };
--- src/database/models/matricula.js
+++ src/database/models/matricula.js
@@ -24,6 +24,7 @@ module.exports = (sequelize, DataTypes) => {
 		sequelize,
 		modelName: 'Matricula',
 		tableName: 'matriculas'
+		paronoid: true
 	});
 	return Matricula;
 };
